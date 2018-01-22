0b223ee507b4b6f60258d2bc7fcf198425cbe6d6
mickeymcc <mccmarci@gmail.com>
2018-01-22 16:13:12 -0600

    "Pass all tests"

--- src/dancer.js
+++ src/dancer.js
@@ -1,6 +1,8 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
-
+  this.timeBetweenSteps = timeBetweenSteps;
+  this.top = top;
+  this.left = left;
   //var dancer = {};
 
   // use jQuery to create an HTML <span> tag
@@ -28,6 +30,6 @@ makeDancer.prototype.setPosition = function(top, left) {
 makeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step.bind(this), timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 

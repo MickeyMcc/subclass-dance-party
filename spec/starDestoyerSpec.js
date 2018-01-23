6f53fa087c80b79d8e74386486649288290f3143
mickeymcc <mccmarci@gmail.com>
2018-01-23 17:00:42 -0600

    Add tests and pass

--- spec/starDestoyerSpec.js
+++ spec/starDestoyerSpec.js
@@ -1,33 +1,32 @@
-describe('Enterprise', function() {
+describe('StarDestoyer', function() {
 
-  var enterprise, clock;
+  var destoyer, clock;
   var timeBetweenSteps = 100;
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    enterprise = new Enterprise(10, 20, timeBetweenSteps);
+    destoyer = new StarDestoyer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
-    expect(enterprise.$node).to.be.an.instanceof(jQuery);
+    expect(destoyer.$node).to.be.an.instanceof(jQuery);
   });
 
   it('should have a step function that makes it move', function() {
-    sinon.spy(enterprise.$node, 'animate');
-    enterprise.step();
-    expect(enterprise.$node.animate.called).to.be.true;
+    sinon.spy(destoyer.$node, 'animate');
+    destoyer.step();
+    expect(destoyer.$node.animate.called).to.be.true;
   });
 
   describe('dance', function() {
     it('should call move at least once per second', function() {
-      sinon.spy(enterprise, 'move');
-      expect(enterprise.move.callCount).to.be.equal(0);
+      sinon.spy(destoyer, 'move');
+      expect(destoyer.move.callCount).to.be.equal(0);
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
 
-      expect(enterprise.move.callCount).to.be.equal(1);
+      expect(destoyer.move.callCount).to.be.equal(1);
       
-
     });
   });
 });
